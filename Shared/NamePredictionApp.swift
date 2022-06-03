de7dba945e0d9461b50f0ec2bdc4bc9bd543e649
Michael Schinis <mschinis@gmail.com>
2022-06-03 12:32:44 +0300

    Initial Commit

---
+++ Shared/NamePredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamePredictionApp.swift
+//  Shared
+//
+//  Created by Michael Schinis on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NamePredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
